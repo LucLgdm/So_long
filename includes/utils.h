2c4af207d07a585f249f409925ff418201d8979b
Luc De Mercey <LucLgdm@users.noreply.github.com>
2024-12-20 11:47:16 +0100

    test 2

--- includes/utils.h
+++ includes/utils.h
@@ -6,7 +6,7 @@
 /*   By: lde-merc <lde-merc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 16:38:08 by lde-merc          #+#    #+#             */
-/*   Updated: 2024/12/20 11:35:08 by lde-merc         ###   ########.fr       */
+/*   Updated: 2024/12/20 11:46:45 by lde-merc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,5 @@
 
 void	print_error_and_exit(void);
 void	free_all(t_world *world);
+
 #endif
--- srcs/free.c
+++ srcs/free.c
@@ -6,7 +6,7 @@
 /*   By: lde-merc <lde-merc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 11:33:49 by lde-merc          #+#    #+#             */
-/*   Updated: 2024/12/20 11:37:13 by lde-merc         ###   ########.fr       */
+/*   Updated: 2024/12/20 11:47:01 by lde-merc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ void	free_all(t_world *world)
 	i = -1;
 	while (world->map->map[++i])
 		free(world->map->map[i]);
-	free(world->map->exit);
-	free(world->map->collect);
-	free(world->map->start_pos);
+	// free(world->map->exit);
+	// free(world->map->collect);
+	// free(world->map->start_pos);
 	free(world->map->map);
 }
